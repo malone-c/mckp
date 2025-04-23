f79a57a360aaa649373f9a60d2f9c2c5fbabd21c
Chris Malone <malone.chrisj@gmail.com>
2025-04-23 13:41:14 +1000

    Move test case, fix and add assert

--- tests/test_mckp.py
+++ tests/test_mckp.py
@@ -1,14 +1,10 @@
+import polars as pl
 import pyarrow as pa
 import mckp
 
-# Create a test ListArray: [[1, 2], [3], [4, 5, 6]]
-# treatment_ids = pa.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]], type=pa.list_(pa.uint32()))
-# rewards = pa.array([[0, 110, 200], [0, 230, 300], [0, 210, 500]], type=pa.list_(pa.float64()))
-# costs = pa.array([[0, 100, 200], [0, 200, 300], [0, 200, 400]], type=pa.list_(pa.float64()))
-# budget_constraint = 500
-
 
 # Create test data with more customers and smaller numbers
+
 treatment_ids = pa.array([
     [0, 1, 2, 3], 
     [0, 1, 2], 
@@ -33,21 +29,40 @@ costs = pa.array([
     [0, 7, 14]
 ], type=pa.list_(pa.float64()))
 
+table = pl.DataFrame({
+    'treatment_id': treatment_ids,
+    'reward': rewards,
+    'cost': costs
+})
+
+# table = pa.Table.from_arrays(
+#     [treatment_ids, rewards, costs],
+#     names=["treatment_id", "reward", "cost"]
+# )
+
+# (
+#     pl.from_arrow(table)
+#         .select(pl.col("treatment_id").explode().unique())
+#         .with_columns(pl.col('treatment_id').rank('dense', descending=True)) # dns should be 0 -- TODO: deal with this explicitly
+# )
+
 budget_constraint = 50
 
 print("Python ListArray:", treatment_ids)
 print("\nC++ Analysis:")
 
-results = mckp.mckp.solve(
-    treatment_ids,
-    rewards,
-    costs,
+solver = mckp.Solver()
+
+results = solver.fit(
+    table,
     budget_constraint,
-    0,
 )
 
-print(dir(results))
+assert results['spend'][-2] == 47.
+assert results['gain'][-2] == 65.
+
 print(results)
 
 print(f"{results['spend'][-2]=}")
 print(f"{results['gain'][-2]=}")
+
